d89dc19eabbeabd6063ee132654fc797aaf51c0f
Roman Rizzi <rizziromanalejandro@gmail.com>
2019-04-10 10:28:23 -0300

    Improve migration to make sure that we only affect akismet logs

--- db/migrate/20190409153209_migrate_dismissed_custom_logs.rb
+++ db/migrate/20190409153209_migrate_dismissed_custom_logs.rb
@@ -1,17 +1,27 @@
 class MigrateDismissedCustomLogs < ActiveRecord::Migration[5.2]
   def up
     DB.exec <<~SQL
-      UPDATE user_histories
+      UPDATE user_histories AS uh
       SET custom_type = 'ignored'
-      WHERE custom_type = 'dismissed' AND action = #{UserHistory.actions[:custom_staff]}
+      FROM post_custom_fields AS pcf
+      WHERE 
+        uh.custom_type = 'dismissed' AND 
+        uh.action = #{UserHistory.actions[:custom_staff]} AND
+        uh.post_id = pcf.post_id AND
+        pcf.name = 'AKISMET_STATE' AND pcf.value = 'dismissed'
     SQL
   end
 
   def down
     DB.exec <<~SQL
-      UPDATE user_histories
+      UPDATE user_histories AS uh
       SET custom_type = 'dimissed'
-      WHERE custom_type = 'ignored' AND action = #{UserHistory.actions[:custom_staff]}
+      FROM post_custom_fields AS pcf
+      WHERE 
+        uh.custom_type = 'ignored' AND 
+        uh.action = #{UserHistory.actions[:custom_staff]} AND
+        uh.post_id = pcf.post_id AND
+        pcf.name = 'AKISMET_STATE' AND pcf.value = 'dismissed'
     SQL
   end
 end
